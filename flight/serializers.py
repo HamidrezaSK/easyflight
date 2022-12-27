bbff558736aa36f7083f746d31588a2586c93c73
Hamidreza Sanaee <shrsanaee@gmail.com>
2022-12-27 23:32:21 +0000

    Add seat flight route airport airplane serializers

--- flight/serializers.py
+++ flight/serializers.py
@@ -1,7 +1,33 @@
 from rest_framework import serializers
-from .models import Airline
+from .models import Airline,Airplane,Airport,Route,Seat,Flight
 
 class AirlineSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airline
+        fields = '__all__'
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airplane
+        fields = '__all__'
+
+class AirportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Airport
+        fields = '__all__'
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = '__all__'
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = '__all__'
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
         fields = '__all__'
