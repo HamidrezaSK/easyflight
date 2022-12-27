bf93c7882ac3826c051b66369425552c7db61b28
Hamidreza Sanaee <shrsanaee@gmail.com>
2022-12-27 20:20:51 +0000

    Add swagger to router

--- ticket/urls.py
+++ ticket/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,url
 from flight.views import AirlineList, AirlineDetail
+from rest_framework.documentation import include_docs_urls
+
+schema_view = get_swagger_view(title="Swagger Docs")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
     path('api/airline/', AirlineList.as_view()),
     path('api/airline/{id}', AirlineDetail.as_view()),
+    url(r'^api/docs/', schema_view),
 ]
