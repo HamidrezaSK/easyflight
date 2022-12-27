c2f961d954869e0975b178a6a359325566642185
Hamidreza Sanaee <shrsanaee@gmail.com>
2022-12-27 23:32:05 +0000

    Add seat flight route airport airplane serializers

--- flight/views.py
+++ flight/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from .serializers import AirlineSerializer
-from .models import Airline
+from .serializers import AirlineSerializer,AirplaneSerializer,AirportSerializer,RouteSerializer,SeatSerializer,FlightSerializer
+from .models import Airline,Airplane,Airport,Route,Seat,Flight
 
 class AirlineList(generics.ListCreateAPIView):
     queryset = Airline.objects.all()
@@ -12,3 +12,44 @@ class AirlineList(generics.ListCreateAPIView):
 class AirlineDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Airline.objects.all()
     serializer_class = AirlineSerializer
+
+class AirplaneList(generics.ListCreateAPIView):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+class AirplaneDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+class AirportList(generics.ListCreateAPIView):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+class AirportDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+class RouteList(generics.ListCreateAPIView):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+class RouteDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+class SeatList(generics.ListCreateAPIView):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class SeatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+class FlightList(generics.ListCreateAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+class FlightDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
